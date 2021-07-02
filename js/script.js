c43785a90eff7de7d9193b863cbed24664fac5a2
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-02 14:06:23 +0200

    init project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//TODO Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+
+
+//TODO Bonus Far comparire gli indirizzi email solamente quando sono stati tutti generati.
