e9485881c9135797de008c67456681fe24309014
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-02 14:09:16 +0200

    Closing project: add bonus

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 //TODO Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+//TODO Bonus Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 const app = new Vue({
   el : '#app',
   data : {
@@ -19,5 +22,3 @@ const app = new Vue({
   },
 });
 
-
-//TODO Bonus Far comparire gli indirizzi email solamente quando sono stati tutti generati.
